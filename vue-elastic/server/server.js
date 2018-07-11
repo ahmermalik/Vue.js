7b4f35c9d24ef755d001f9ffd3fdec63dce0c301
Ahmer Malik <ahmer9800@gmail.com>
2018-07-11 00:38:52 -0500

    update comments

--- vue-elastic/server/server.js
+++ vue-elastic/server/server.js
@@ -8,3 +8,7 @@ const PORT = 5000;
 app.listen(PORT, function() {
     console.log('Listening on port: ',PORT);
 });
+
+// Turn on/off elasticsearch
+//brew services start elasticsearch
+//brew services stop elasticsearch
